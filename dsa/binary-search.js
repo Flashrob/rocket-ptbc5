79f457534c056b5db26c7689463c663d38974c1c
Flashrob <kolsek.income@gmail.com>
2022-08-28 01:48:13 +0800

    add binary search

---
+++ dsa/binary-search.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function (nums, target) {
+  let leftIndex = 0;
+  let rightIndex = nums.length - 1;
+
+  while (leftIndex <= rightIndex) {
+    const midIndex = Math.floor((leftIndex + rightIndex) / 2);
+
+    if (nums[midIndex] === target) return midIndex;
+
+    if (nums[midIndex] < target) {
+      leftIndex = midIndex + 1;
+    } else if (nums[midIndex] > target) {
+      rightIndex = midIndex - 1;
+    }
+  }
+
+  return -1;
+};
+
+//https://leetcode.com/problems/binary-search/
