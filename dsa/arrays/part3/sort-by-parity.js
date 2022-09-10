21d9b886a255ca7f25cfbfc95e10f5ea84f95412
Flashrob <38381396+Flashrob@users.noreply.github.com>
2022-09-11 00:33:12 +0800

    DSA: add sort by parity solution

---
+++ dsa/arrays/part3/sort-by-parity.js
@@ -0,0 +1,13 @@
+// https://leetcode.com/problems/sort-array-by-parity
+
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+ var sortArrayByParity = function(nums) {
+    return nums.sort((a, b) => {
+        if (a % 2 === 0) {
+            return -1;
+        }
+    })
+};
