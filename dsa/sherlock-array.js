1f848f2e74839e3b48ff5d0012036dafc520519a
Flashrob <38381396+Flashrob@users.noreply.github.com>
2022-09-10 11:34:51 +0800

    Update sherlock-array.js

--- dsa/sherlock-array.js
+++ dsa/sherlock-array.js
@@ -1,24 +1,18 @@
 // https://www.hackerrank.com/challenges/sherlock-and-array/problem?isFullScreen=true
 function balancedSums(arr) {
-    const length = arr.length;
+    if(arr.length === 1) return 'YES';
+    if(arr.length === 2) return 'NO';
     
-    for (let i = 0; i < length; i++) {
-        let leftSum = 0;
-        let rightSum = 0;
-        const element = arr[i];
-
-        if (i > 0) {
-            for (let j = 0; j < i; j++) {
-                leftSum += arr[j];
-            } 
-        }
-  
-        for (let k = i + 1; k < length; k++) {
-            rightSum += arr[k];
-        }
+    let leftSum = 0;
+    let rightSum = arr.slice(1).reduce((acc, cur) => acc + cur, 0);
+    if(leftSum === rightSum) return 'YES';
+    
+    for(let i = 0; i < arr.length; i++) {
+        leftSum += arr[i];
+        rightSum -= arr[i+1];
         
-        if (leftSum === rightSum) return "YES"
+        if(leftSum === rightSum) return 'YES';
     }
     
-    return "NO"
+    return 'NO';
 }
